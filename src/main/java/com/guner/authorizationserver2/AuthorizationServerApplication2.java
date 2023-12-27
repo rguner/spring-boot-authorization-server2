41c9d6a2648a954a70d10264b29b9251a5767cb2
Ramazan Guner <ramazan.guner@haud.com>
2023-12-27 17:05:42 +0300

    web link

--- src/main/java/com/guner/authorizationserver2/AuthorizationServerApplication2.java
+++ src/main/java/com/guner/authorizationserver2/AuthorizationServerApplication2.java
@@ -3,6 +3,7 @@ package com.guner.authorizationserver2;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// https://www.appsdeveloperblog.com/spring-authorization-server-tutorial/
 @SpringBootApplication
 public class AuthorizationServerApplication2 {
 
