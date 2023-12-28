34c2e9e9d3dec176d4daf8889720c484b9060472
Ramazan Guner <ramazan.guner@haud.com>
2023-12-28 10:48:39 +0300

    csrf removed

--- src/main/java/com/guner/authorizationserver2/config/AuthorizationServerConfiguration.java
+++ src/main/java/com/guner/authorizationserver2/config/AuthorizationServerConfiguration.java
@@ -6,6 +6,7 @@ import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.oauth2.core.AuthorizationGrantType;
 import org.springframework.security.oauth2.core.ClientAuthenticationMethod;
 import org.springframework.security.oauth2.core.oidc.OidcScopes;
--- src/main/java/com/guner/authorizationserver2/config/SpringSecurityConfiguration.java
+++ src/main/java/com/guner/authorizationserver2/config/SpringSecurityConfiguration.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -22,6 +23,7 @@ public class SpringSecurityConfiguration {
 
         http
                 .authorizeHttpRequests(authorizeRequests -> authorizeRequests.anyRequest().authenticated())
+                .csrf(AbstractHttpConfigurer::disable) // login post'da csrf beklemesin.
                 .formLogin(Customizer.withDefaults());
 
         return http.build();
