fb260a54b5a428530ea44fd38e6beaddeef1bd59
Ramazan Guner <ramazan.guner@haud.com>
2023-12-28 17:10:39 +0300

    scopes added

--- src/main/java/com/guner/authorizationserver2/config/AuthorizationServerConfiguration.java
+++ src/main/java/com/guner/authorizationserver2/config/AuthorizationServerConfiguration.java
@@ -38,7 +38,10 @@ public class AuthorizationServerConfiguration {
                 .redirectUri("http://127.0.0.1:8080/login/oauth2/code/users-client-oidc")
                 .redirectUri("http://127.0.0.1:8080/authorized")
                 .scope(OidcScopes.OPENID)
+                .scope(OidcScopes.PROFILE)
+                .scope(OidcScopes.EMAIL)
                 .scope("read")
+                .scope("write")
                 //.clientSettings(ClientSettings.builder().requireAuthorizationConsent(true).build())
                 .build();
 
